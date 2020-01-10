18cc57afd19e4f0f90c3fdcec159c85b03d1d642
Aaryan-kapur <44099242+Aaryan-kapur@users.noreply.github.com>
2020-01-10 16:32:23 +0530

    edit-bhaavya-8

--- client/src/scripts/colorlib-regform-3/index.jsx
+++ client/src/scripts/colorlib-regform-3/index.jsx
@@ -10,9 +10,29 @@ import ReCAPTCHA from 'react-google-recaptcha';
 function onChange(value) {
   console.log('Captcha value:', value);
 }
+function handleChange(event) {
+  this.setState(([event.target.name] = event.target.value));
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
 
+  fetch('url daal chutiye', {
+    method: 'POST',
+    body: data
+  });
+}
 class Form extends Component {
-  state = {};
+  constructor() {
+    this.state = {
+      name: ' ',
+      email: ' ',
+      institution: ' ',
+      email: ' ',
+      Econtact: ' '
+    };
+  }
+
   render() {
     return (
       <div class="page-wrapper mainForm bg-white card1 p-t-80 p-b-80 font-poppins">
@@ -28,6 +48,7 @@ class Form extends Component {
                     type="text"
                     placeholder="Name"
                     name="name"
+                    onChange={event => this.handleChange(event)}
                   />
                 </div>
                 <div class="input-group">
@@ -36,6 +57,7 @@ class Form extends Component {
                     type="text"
                     placeholder="Institution Name"
                     name="institution"
+                    onChange={event => this.handleChange(event)}
                   />
                 </div>
                 <div class="input-group">
@@ -44,6 +66,7 @@ class Form extends Component {
                     type="number"
                     placeholder="Contact Number"
                     name="contact"
+                    onChange={event => this.handleChange(event)}
                   />
                 </div>
                 <div class="input-group">
@@ -52,6 +75,7 @@ class Form extends Component {
                     type="email"
                     placeholder="Email Address"
                     name="email"
+                    onChange={event => this.handleChange(event)}
                   />
                 </div>
                 <div class="input-group">
@@ -60,9 +84,13 @@ class Form extends Component {
                     type="number"
                     placeholder="Emergency Contact"
                     name="Econtact"
+                    onChange={event => this.handleChange(event)}
                   />
                 </div>
-                <ReCAPTCHA sitekey="6LeQD84UAAAAADuFNXidqZRUpfxFqPeRZLm9StEZ" onChange={onChange} />
+                <ReCAPTCHA
+                  sitekey="6LeQD84UAAAAADuFNXidqZRUpfxFqPeRZLm9StEZ"
+                  onChange={onChange}
+                />
                 <div class="p-t-10">
                   <button class="btn btn--pill btn--green" type="submit">
                     Submit
