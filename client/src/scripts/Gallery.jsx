513c38969f6521745392b9a4975728ba4799f18f
aaryan-kapur <ak6392@bennett.edu.in>
2020-02-02 21:18:01 +0530

    edit-add-images

--- client/src/scripts/Gallery.jsx
+++ client/src/scripts/Gallery.jsx
@@ -1,136 +1,91 @@
-import React, { Component } from 'react';
-import '../styles/Gallery.scss';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Slider from 'react-slick';
-
+import React, { Component } from "react";
+import "../styles/Gallery.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import image_1 from "../assets/image_1.jpg";
+import image_2 from "../assets/image_2.jpg";
+import image_3 from "../assets/image_3.jpg";
+import image_4 from "../assets/image_4.jpg";
+import image_5 from "../assets/image_5.jpg";
+import image_6 from "../assets/image_6.jpg";
 class Gallery extends Component {
-	state = {};
-	render() {
-		const settings = {
-			className: 'center',
-			centerMode: true,
-			dots: true,
-			// fade: true,
-			infinite: true,
-			centerPadding: '60px',
-			slidesToShow: 3,
-			speed: 500
-		};
+  state = {};
+  render() {
+    const settings = {
+      className: "center",
+      centerMode: true,
+      dots: true,
+      // fade: true,
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 3,
+      speed: 500
+    };
 
-		const settings1 = {
-			className: 'center',
-			// centerMode: true,
-			dots: true,
-			// fade: true,
-			infinite: true,
-			centerPadding: '60px',
-			slidesToShow: 1,
-			speed: 500
-		};
-		return (
-			<div className='Gallery1'>
-				<span className='heading headingGallery  link--kukuri'>
-					Gallery
-				</span>
-				<div className='GalleryOuter'>
-					<div className='onPC'>
-						<Slider {...settings}>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage'
-								/>
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage'
-								/>{' '}
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage'
-								/>{' '}
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage'
-								/>{' '}
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage'
-								/>{' '}
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage'
-								/>{' '}
-							</div>
-						</Slider>
-					</div>
+    const settings1 = {
+      className: "center",
+      // centerMode: true,
+      dots: true,
+      // fade: true,
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 1,
+      speed: 500
+    };
+    return (
+      <div className="Gallery1">
+        <span className="heading headingGallery  link--kukuri">Gallery</span>
+        <div className="GalleryOuter">
+          <div className="onPC">
+            <Slider {...settings}>
+              <div>
+                <img src={image_1} alt="" className="masonryImage" />
+              </div>
+              <div>
+                <img src={image_2} alt="" className="masonryImage" />{" "}
+              </div>
+              <div>
+                <img src={image_3} alt="" className="masonryImage" />{" "}
+              </div>
+              <div>
+                <img src={image_4} alt="" className="masonryImage" />{" "}
+              </div>
+              <div>
+                <img src={image_5} alt="" className="masonryImage" />{" "}
+              </div>
+              <div>
+                <img src={image_6} alt="" className="masonryImage" />{" "}
+              </div>
+            </Slider>
+          </div>
 
-					<div className='onPhone'>
-						<Slider {...settings1}>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage onPhoneImage'
-								/>
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage onPhoneImage'
-								/>{' '}
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage onPhoneImage'
-								/>{' '}
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage onPhoneImage'
-								/>{' '}
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage onPhoneImage'
-								/>{' '}
-							</div>
-							<div>
-								<img
-									src='https://images.unsplash.com/photo-1492684223066-81342ee5ff30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
-									alt=''
-									className='masonryImage onPhoneImage'
-								/>{' '}
-							</div>
-						</Slider>
-					</div>
-				</div>
-			</div>
-		);
-	}
+          <div className="onPhone">
+            <Slider {...settings1}>
+              <div>
+                <img src={image_1} alt="" className="masonryImage" />
+              </div>
+              <div>
+                <img src={image_2} alt="" className="masonryImage" />{" "}
+              </div>
+              <div>
+                <img src={image_3} alt="" className="masonryImage" />{" "}
+              </div>
+              <div>
+                <img src={image_4} alt="" className="masonryImage" />{" "}
+              </div>
+              <div>
+                <img src={image_5} alt="" className="masonryImage" />{" "}
+              </div>
+              <div>
+                <img src={image_6} alt="" className="masonryImage" />{" "}
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Gallery;
